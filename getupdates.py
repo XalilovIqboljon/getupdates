0bd04eb5164027f8dded75ebbe3befd0f6f909c9
Xalilov <eljjey@gmail.com>
2021-01-31 17:32:19 +0500

    f_,l_name

--- getupdates.py
+++ getupdates.py
@@ -1,11 +1,13 @@
 import requests
 from pprint import pprint
-token = '1687972442:AAFTgtZXybvpDqsTddplgtzO9GHUuenLwKs'
+token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
 # print(r.url)
 data = r.json()
 # pprint(data)
-for i in range(len(data)):
-    updates = data['result'][i]['message']['from']
-    print(updates)
+for i in range(len(data['result'])):
+    f_name=data['result'][i]['message']['from'].get('first_name','')
+    l_name=data['result'][i]['message']['from'].get('last_name','')
+    updates =f'{f_name} {l_name}'
+    pprint(updates)
